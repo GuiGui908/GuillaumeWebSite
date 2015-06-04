8233bf4504e061cb2dfb5e2ffc677aafa7d72525
GuiGui908 <guigui908b@gmail.com>
2015-06-04 14:31:29 -0400

    Test conection BD

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -15,7 +15,7 @@ class Controller {
 		try {
 			$DB = new PDO('mysql:host=mysql.hostinger.fr;dbname=u636759449_main;charset=utf8', 'u636759449_gui', 'owk2zeNCRI4r');
 		} catch(Exception $e) {
-			die('Erreur : ' . $e->getMessage());
+			echo 'Erreur : '.$e->getMessage();
 		}
 	}
 
